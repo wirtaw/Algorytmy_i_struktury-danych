023a17aba823bc8ea501c379a86bd3132a0632ea
VValdemarJS <115026060+VValdemarJS@users.noreply.github.com>
2022-10-12 01:09:54 +0300

    Create Zadanie_5.c

---
+++ Laboratorium/2022-10-11/Zadanie_5.c
@@ -0,0 +1,52 @@
+// Valdemar Kudžma
+// Laboratorium 1.
+// Obliczyć funkcja y = ax^2, gdy a - stałe, x jest wybierany z tablicy X(n), wyniki funkcji zapisywać Y(n)
+// Data 2022-10-11
+
+#include <stdio.h>
+
+float a;
+float y;
+int n;
+
+int main(){
+  
+  do {
+    printf("Podaj stala a:\n");
+    scanf("%f", &a);
+  } while (a == 0);
+
+  do {
+    printf("\nPodaj rozmiar tablicy X(n):\n");
+    scanf("%d", &n);
+  } while (n < 0);
+
+  float x[n];
+  float y[n];
+
+  printf("\n");
+  
+  for(int i =0; i < n; i++){
+    printf("wprowadz liczba X(%d) = ", i);
+    scanf("%f", &x[i]);
+  }
+
+  printf("\n\n");
+
+  printf("Wartość stałej a: %0.2f", a);
+  
+  printf("\n");
+
+  for(int i = 0; i < n; i++){
+    printf("Wartosc w tablicy X(%d): %0.2f\n", i, x[i]);
+  }
+
+  printf("\n");
+
+  for(int i = 0; i < n; i++){
+    y[i] = a * x[i] * x[i];
+    printf("y = %0.2f\n", y[i]);
+  }
+  
+  return 0;
+}
