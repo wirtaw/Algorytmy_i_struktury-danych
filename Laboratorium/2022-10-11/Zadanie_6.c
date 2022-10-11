b9186f8f4a6effedc31e35d39b84725f5d70690c
VValdemarJS <115026060+VValdemarJS@users.noreply.github.com>
2022-10-12 02:07:00 +0300

    Update Zadanie_6.c

--- Laboratorium/2022-10-11/Zadanie_6.c
+++ Laboratorium/2022-10-11/Zadanie_6.c
@@ -53,27 +53,30 @@ int main(){
   int ilosc = 0;
   float mnozx = 1;
   float mnozy = 1;
+  int sizex = 0;
+  int sizey = 0;
   
     for(int i = 0; i < n; i++) {
       sumax += x[i];
       sumay += y[i];
-      ilosc++;
       mnozx *= x[i];
       mnozy *= y[i];
      }
+  
+  sizex = sizeof(x)/sizeof(float);
+  sizey = sizeof(y)/sizeof(float);
 
   printf("\nSuma tablicy X(n): %0.2f\n", sumax);
   printf("Suma tablicy Y(n): %0.2f\n", sumay);
-  printf("Ilość elementów tablicy: %d\n", ilosc);
+  printf("Ilość elementów tablicy: %d\n", sizex);
   printf("Mnożenie elementów tablicy X(n): %0.2f\n", mnozx);
   printf("Mnożenie elementów tablicy Y(n): %0.2f\n", mnozy);
   
-  float sredniax = sumax/ilosc;
-  float sredniay - sumay/ilosc;
+  float sredniax = sumax/sizex;
+  float sredniay = sumay/sizey;
   
-  printf("Srednia X(n): %0.2f", sredniax);
-  printf("Srednia Y(n): %0.2f", sredniay);
+  printf("\nSrednia X(n): %0.2f\n", sredniax);
+  printf("Srednia Y(n): %0.2f\n", sredniay);
   
   return 0;
 }
-
