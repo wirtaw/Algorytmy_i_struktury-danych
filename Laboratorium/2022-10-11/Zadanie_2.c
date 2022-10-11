4ffcd79b6b7dc352b5653b07d3bba9018ca290dc
VValdemarJS <115026060+VValdemarJS@users.noreply.github.com>
2022-10-11 21:52:31 +0300

    Update Zadanie_2.c

--- Laboratorium/2022-10-11/Zadanie_2.c
+++ Laboratorium/2022-10-11/Zadanie_2.c
@@ -12,34 +12,43 @@ float krokh = 0;
 int n=0;
 float y;
 
-
 int main(void) {
-  
-  printf("Podaj stala a:\n");
-  scanf("%f", &a);
 
+  do {
+  printf("Podaj stala a:\n"); // if (a != )
+  scanf("%f", &a);
+    }
+    while (a == 0);
+  
 printf("\nPodaj od ilu zwiekszac x:\n");
   scanf("%f", &x_od);
   
-  printf("\nPodaj do ilu zwiekszac x:\n");
-  scanf("%f", &x_do);
+   // do > od od spr
+  do {
+    printf("\nPodaj do ilu zwiekszac x:\n");
+    scanf("%f", &x_do);
+    }
+    while (x_od > x_do);
   
-  printf("\nPodaj krok zwiekszenia x:\n");
+  do{
+  printf("\nPodaj krok zwiekszenia x:\n"); // nie ujemny spr
   scanf("%f", &krokh);
-
-  printf("\nZnaczenie stalej a: %f", a);
-  printf("\nX bedziemy zwiekszac od: %f", x_od);
-  printf("\nX bedziemy zwiekszac do: %f\n", x_do);
-  printf("Znaczenie kroku zwiekszenia x: %f\n", krokh);
+    }
+    while (krokh <= 0);
+  
+  printf("\nZnaczenie stalej a: %0.2f", a);
+  printf("\nX bedziemy zwiekszac od: %0.2f", x_od);
+  printf("\nX bedziemy zwiekszac do: %0.2f\n", x_do);
+  printf("Znaczenie kroku zwiekszenia x: %0.2f\n", krokh);
   
   if (x_do > x_od){
     x = x_od;
     while(x < x_do) {
       y = a * x * x;
-      printf("\ny = %f\nx = %f\n", y, x);
+      printf("\ny = %0.2f\nx = %0.2f\n", y, x);
       x += krokh;
     }
-  } 
+  }
   
   return 0;
 }
