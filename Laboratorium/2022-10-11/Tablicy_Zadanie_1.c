64d568b8a7d93e6754306cdae7349338ff244d4d
VValdemarJS <115026060+VValdemarJS@users.noreply.github.com>
2022-10-12 23:43:20 +0300

    Update Tablicy_Zadanie_1.c

--- Laboratorium/2022-10-11/Tablicy_Zadanie_1.c
+++ Laboratorium/2022-10-11/Tablicy_Zadanie_1.c
@@ -3,11 +3,12 @@
 // Data 2022-10-11
 
 #include <stdio.h>
-float x = 2;
-float a = 1;
-float y = 0;
 
 int main(void) {
+  
+float x = 2;
+float a = 1;
+float y = 0;  
   
   while(x<7){
     y = a*x*x;
@@ -15,5 +16,6 @@ int main(void) {
     x += 2;
     a += 0.5;
   }
+  
   return 0;
 }
