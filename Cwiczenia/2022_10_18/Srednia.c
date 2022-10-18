faab66ddf33de9d4a4f3e5fefde5af2711b90da8
VValdemarJS <115026060+VValdemarJS@users.noreply.github.com>
2022-10-18 19:19:08 +0300

    Update Srednia.c

--- Cwiczenia/2022_10_18/Srednia.c
+++ Cwiczenia/2022_10_18/Srednia.c
@@ -1,1 +1,11 @@
+// Pseudokod i udowodnienie że jest częściowo poprawny 
 
+srednia(ilosc_wartosci, znaczenie_wartosci[]){
+	i = 0;
+	while (i < ilosc_wartosci){
+		suma += znaczenie_wartosci[i];
+		i++;
+	}	
+	rezultat = suma / ilosc_wartosc;
+	return rezultat;
+}
