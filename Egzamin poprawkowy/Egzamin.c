07d1e94dff891a6cb035d69209fcac0f530a06ee
VValdemarJS <115026060+VValdemarJS@users.noreply.github.com>
2023-02-22 17:21:08 +0200

    Update Egzamin.c

--- Egzamin poprawkowy/Egzamin.c
+++ Egzamin poprawkowy/Egzamin.c
@@ -4,27 +4,29 @@ int i,j,k,n;
 
 void staircase(int n){
 	
-  for(i=1;i<=n;i++)
-	{
-    for(k=n-i;k>0;k--)
-        {
-          printf(" ");
-        }
-		for(j=1;j<=i;j++)
-		  {
-		  	printf("#");
-		  }
-	  printf("\n");
+for(i=1;i<=n;i++)
+{
     
-	}
+    for(k=n-i;k>0;k--)
+    {
+    	printf(" ");
+    }
+	  
+    for(j=1;j<=i;j++)
+    {
+	printf("#");
+    }
+	
+    printf("\n");
+    
+}
   
 }
 
 int main()
 {
-	
 staircase(10);
-  
+return 0;
 }
 			
 			
